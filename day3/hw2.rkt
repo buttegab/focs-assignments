30132892cf003803e7890cab66d41d32562273ae
buttegab <gabriel.butterick@students.olin.edu>
2016-09-11 23:38:52 -0400

    completed homework2

--- day3/hw2.rkt
+++ day3/hw2.rkt
@@ -1,34 +1,79 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Gabriel Butterick
 ;;;
 ;;; Check one:
 ;;; [ ] I completed this assignment without assistance or external resources.
-;;; [ ] I completed this assignment with assistance from ___
+;;; [x] I completed this assignment with assistance from Joe Sutker, who helped me find a bug in problem 2.
 ;;;     and/or using these external resources: ___
 
 ;;; 1.  Create a calculator that takes one argument: a list that represents an expression.
 
 (define (calculate x)
-  your-code-here)
+  ;(display x) (newline)
+  (if (eq? (first x) 'ADD) (my_addition (rest x)) (if (eq? (first x) 'MUL) (my_multiplication (rest x)) (if (eq? (first x) 'SUB)
+   (my_subtraction (rest x)) (if (eq? (first x) 'DIV) (my_division (rest x)) (if (eq? (first x) 'GT) (greater_than (rest x))
+   (if (eq? (first x) 'LT) (less_than (rest x)) (if (eq? (first x) 'EQ) (equal_to (rest x)) (if (eq? (first x) 'ANND)
+   (ander (rest x)) (if (eq? (first x) 'ORR) (orer (rest x)) (if (eq? (first x) 'NOTT) (noter (rest x)) (ifer (rest x)))))))))))))
+(define (my_addition x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (+ (first x) (my_addition (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+  (+ (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+(define (my_multiplication x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (* (first x) (my_multiplication (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+   (* (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+(define (my_subtraction x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (- (first x) (my_subtraction (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+  (- (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+(define (my_division x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (/ (first x) (my_division (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+  (/ (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+(define (greater_than x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (> (first x) (greater_than (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+  (> (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+(define (less_than x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (< (first x) (less_than (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+  (< (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+(define (equal_to x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (= (first x) (equal_to (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+  (= (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+(define (ander x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (and (first x) (ander (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+  (and (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+(define (orer x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (or (first x) (orer (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+  (or (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+(define (noter x)
+  (if (integer? (first x)) (if (eq? (rest x) '()) (first x) (not (first x) (noter (rest x)))) (if (eq? (rest x) '()) (calculate (first x))
+  (not (calculate (first x)) (if (integer? (first(rest x))) (first(rest x)) (calculate (first(rest x))))))))
+
+;if calculate x[0] then calculate x[1] else calulate x[2]
+(define (ifer x)
+  (if (calculate (list-ref x 0)) (calculate (list-ref x 1)) (calculate(list-ref x 2))))
+
+
+
 
 (calculate '(ADD 3 4)) ;; --> 7
 
 ;;; 2. Expand the calculator's operation to allow for arguments that are themselves well-formed arithmetic expressions.
 
-(calculate '(ADD 3 (MUL 4 5))) ;; --> 23   ;; what is the equivalent construction using list?
-(calculate '(SUB (ADD 3 4) (MUL 5 6))) ;; --> -23
 
-;;; 3. Add comparators returning booleans (*e.g.*, greater than, less than, …).
-;; Note that each of these takes numeric arguments (or expressions that evaluate to produce numeric values),
-;; but returns a boolean.  We suggest operators `GT`, `LT`, `GE`, `LE`, `EQ`, `NEQ`.
 
 	(calculate '(GT (ADD 3 4) (MUL 5 6))) ;; --> #f
 	(calculate '(LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6))))) ;; --> #t
 
 ;;; 4. Add boolean operations ANND, ORR, NOTT
 
-(calculate '(AND (GT (ADD 3 4) (MUL 5 6))) (LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6)))))) ;; --> #f
+(calculate '(ANND (GT (ADD 3 4) (MUL 5 6)) (LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6)))))) ;; --> #f
 
 ;;; 5. Add IPH
 
